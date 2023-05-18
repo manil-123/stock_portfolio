cb6cd288a10fe7894b3405249be887f7d9f361ba
Manil Pun <punmanil3@gmail.com>
2023-05-18 10:11:39 +0545

    top losers scrapped

--- lib/services/scrapper.dart
+++ lib/services/scrapper.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
@@ -60,7 +59,35 @@ class Scrapper {
       topGainersMapList.add(individualMap);
     }
     topGainersMapList.removeAt(0);
-    log(topGainersMapList.toString());
     return {"top_gainers": topGainersMapList};
+  }
+
+  Future<Map<String, dynamic>> fetchTopLosersData() async {
+    final url = Uri.parse(URLConstants.SCRAP_URL);
+    final response = await http.get(url);
+    final html = parse(response.body);
+    final topLosersData =
+        html.querySelector('#ctl00_ContentPlaceHolder1_LiveLosers')!;
+    final topLosersRow = topLosersData.querySelectorAll('tr');
+    List<Map<String, dynamic>> topLosersMapList = [];
+    for (var row in topLosersRow) {
+      final rowData = row.querySelectorAll('td');
+      Map<String, dynamic> individualMap = {};
+      if (rowData.isNotEmpty) {
+        final title = rowData[0]
+            .querySelector('a')
+            ?.attributes['title']
+            ?.split('(')[1]
+            .replaceAll(")", "");
+        individualMap['companyName'] = title;
+        individualMap['symbol'] = rowData[0].text.toString();
+        individualMap['ltp'] = rowData[1].text.toString();
+        individualMap['change'] = rowData[2].text.toString();
+        individualMap['quantity'] = rowData[6].text.toString();
+      }
+      topLosersMapList.add(individualMap);
+    }
+    topLosersMapList.removeAt(0);
+    return {"top_losers": topLosersMapList};
   }
 }
