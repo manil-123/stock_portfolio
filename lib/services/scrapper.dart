dc998f156bba8bc33babfb3c3175acf8d507a996
Manil Pun <manil.pun@ebpearls.com>
2024-03-10 11:33:26 +0545

    exception handling on server error.

--- lib/services/scrapper.dart
+++ lib/services/scrapper.dart
@@ -6,119 +6,149 @@ import 'package:share_portfolio/core/constants/constants.dart';
 @LazySingleton()
 class Scrapper {
   Future<Map<String, dynamic>> fetchNepsePriceHistory() async {
-    final url = Uri.parse(URLConstants.NEPSE_PRICE_HISTORY_URL);
-    final response = await http.get(url);
-    final html = parse(response.body);
-    final historyData =
-        html.querySelector("#ctl00_ContentPlaceHolder1_divData")!;
-    final historyDataRow = historyData.querySelectorAll('tr');
-    List<Map<String, dynamic>> historyDataMapList = [];
-    for (var row in historyDataRow) {
-      final rowData = row.querySelectorAll('td');
-      Map<String, dynamic> individualMap = {};
-      if (rowData.isNotEmpty) {
-        individualMap['date'] = rowData[1].text.toString();
-        individualMap['index'] = double.parse(
-          rowData[2].text.toString().replaceAll(',', ''),
-        );
-        individualMap['pointChange'] = double.parse(
-          rowData[3].text.toString().replaceAll('%', ''),
-        );
-        individualMap['percentageChange'] = double.parse(
-          rowData[4].text.toString().replaceAll('%', ''),
-        );
+    try {
+      final url = Uri.parse(URLConstants.NEPSE_PRICE_HISTORY_URL);
+      final response = await http.get(url);
+      final html = parse(response.body);
+      final historyData =
+          html.querySelector("#ctl00_ContentPlaceHolder1_divData")!;
+      final historyDataRow = historyData.querySelectorAll('tr');
+      List<Map<String, dynamic>> historyDataMapList = [];
+      for (var row in historyDataRow) {
+        final rowData = row.querySelectorAll('td');
+        Map<String, dynamic> individualMap = {};
+        if (rowData.isNotEmpty) {
+          individualMap['date'] = rowData[1].text.toString();
+          individualMap['index'] = double.parse(
+            rowData[2].text.toString().replaceAll(',', ''),
+          );
+          individualMap['pointChange'] = double.parse(
+            rowData[3].text.toString().replaceAll('%', ''),
+          );
+          individualMap['percentageChange'] = double.parse(
+            rowData[4].text.toString().replaceAll('%', ''),
+          );
+        }
+        historyDataMapList.add(individualMap);
       }
-      historyDataMapList.add(individualMap);
-    }
-    historyDataMapList.removeAt(0);
+      historyDataMapList.removeAt(0);
 
-    return {
-      "price_history": historyDataMapList,
-    };
+      return {
+        "price_history": historyDataMapList,
+      };
+    } catch (e) {
+      return {
+        "price_history": <Map<String, dynamic>>[],
+      };
+    }
   }
 
   Future<Map<String, dynamic>> fetchStockData() async {
-    final url = Uri.parse(URLConstants.SCRAP_URL);
-    final response = await http.get(url);
-    final html = parse(response.body);
-    final tradingData = html.querySelector('#live-trading')!;
-    final tradingRow = tradingData.querySelectorAll('tr');
-    List<Map<String, dynamic>> tradingMapList = [];
-    for (var row in tradingRow) {
-      final rowData = row.querySelectorAll('td');
-      Map<String, dynamic> individualMap = {};
-      if (rowData.isNotEmpty) {
-        final title = rowData[0]
-            .querySelector('a')
-            ?.attributes['title']
-            ?.split('(')[1]
-            .replaceAll(")", "");
-        individualMap['companyName'] = title;
-        individualMap['symbol'] = rowData[0].text.toString();
-        individualMap['ltp'] = rowData[1].text.toString();
-        individualMap['change'] = rowData[2].text.toString();
+    try {
+      final url = Uri.parse(URLConstants.SCRAP_URL);
+      final response = await http.get(url);
+      final html = parse(response.body);
+      final tradingData = html.querySelector('#live-trading')!;
+      final tradingRow = tradingData.querySelectorAll('tr');
+      List<Map<String, dynamic>> tradingMapList = [];
+      for (var row in tradingRow) {
+        final rowData = row.querySelectorAll('td');
+        Map<String, dynamic> individualMap = {};
+        if (rowData.isNotEmpty) {
+          final title = rowData[0]
+              .querySelector('a')
+              ?.attributes['title']
+              ?.split('(')[1]
+              .replaceAll(")", "");
+          individualMap['companyName'] = title;
+          individualMap['symbol'] = rowData[0].text.toString();
+          individualMap['ltp'] = rowData[1].text.toString();
+          individualMap['change'] = rowData[2].text.toString();
+        }
+        tradingMapList.add(individualMap);
       }
-      tradingMapList.add(individualMap);
+      tradingMapList.removeAt(0);
+      return {
+        "list": tradingMapList,
+      };
+    } catch (e) {
+      return {
+        "list": <Map<String, dynamic>>[],
+      };
     }
-    tradingMapList.removeAt(0);
-    return {"list": tradingMapList};
   }
 
   Future<Map<String, dynamic>> fetchTopGainersData() async {
-    final url = Uri.parse(URLConstants.SCRAP_URL);
-    final response = await http.get(url);
-    final html = parse(response.body);
-    final topGainersData =
-        html.querySelector('#ctl00_ContentPlaceHolder1_LiveGainers')!;
-    final topGainersRow = topGainersData.querySelectorAll('tr');
-    List<Map<String, dynamic>> topGainersMapList = [];
-    for (var row in topGainersRow) {
-      final rowData = row.querySelectorAll('td');
-      Map<String, dynamic> individualMap = {};
-      if (rowData.isNotEmpty) {
-        final title = rowData[0]
-            .querySelector('a')
-            ?.attributes['title']
-            ?.split('(')[1]
-            .replaceAll(")", "");
-        individualMap['companyName'] = title;
-        individualMap['symbol'] = rowData[0].text.toString();
-        individualMap['ltp'] = rowData[1].text.toString();
-        individualMap['change'] = rowData[2].text.toString();
-        individualMap['quantity'] = rowData[6].text.toString();
+    try {
+      final url = Uri.parse(URLConstants.SCRAP_URL);
+      final response = await http.get(url);
+      final html = parse(response.body);
+      final topGainersData =
+          html.querySelector('#ctl00_ContentPlaceHolder1_LiveGainers')!;
+      final topGainersRow = topGainersData.querySelectorAll('tr');
+      List<Map<String, dynamic>> topGainersMapList = [];
+      for (var row in topGainersRow) {
+        final rowData = row.querySelectorAll('td');
+        Map<String, dynamic> individualMap = {};
+        if (rowData.isNotEmpty) {
+          final title = rowData[0]
+              .querySelector('a')
+              ?.attributes['title']
+              ?.split('(')[1]
+              .replaceAll(")", "");
+          individualMap['companyName'] = title;
+          individualMap['symbol'] = rowData[0].text.toString();
+          individualMap['ltp'] = rowData[1].text.toString();
+          individualMap['change'] = rowData[2].text.toString();
+          individualMap['quantity'] = rowData[6].text.toString();
+        }
+        topGainersMapList.add(individualMap);
       }
-      topGainersMapList.add(individualMap);
+      topGainersMapList.removeAt(0);
+      return {
+        "top_gainers": topGainersMapList,
+      };
+    } catch (e) {
+      return {
+        "top_gainers": <Map<String, dynamic>>[],
+      };
     }
-    topGainersMapList.removeAt(0);
-    return {"top_gainers": topGainersMapList};
   }
 
   Future<Map<String, dynamic>> fetchTopLosersData() async {
-    final url = Uri.parse(URLConstants.SCRAP_URL);
-    final response = await http.get(url);
-    final html = parse(response.body);
-    final topLosersData =
-        html.querySelector('#ctl00_ContentPlaceHolder1_LiveLosers')!;
-    final topLosersRow = topLosersData.querySelectorAll('tr');
-    List<Map<String, dynamic>> topLosersMapList = [];
-    for (var row in topLosersRow) {
-      final rowData = row.querySelectorAll('td');
-      Map<String, dynamic> individualMap = {};
-      if (rowData.isNotEmpty) {
-        final title = rowData[0]
-            .querySelector('a')
-            ?.attributes['title']
-            ?.split('(')[1]
-            .replaceAll(")", "");
-        individualMap['companyName'] = title;
-        individualMap['symbol'] = rowData[0].text.toString();
-        individualMap['ltp'] = rowData[1].text.toString();
-        individualMap['change'] = rowData[2].text.toString();
-        individualMap['quantity'] = rowData[6].text.toString();
+    try {
+      final url = Uri.parse(URLConstants.SCRAP_URL);
+      final response = await http.get(url);
+      final html = parse(response.body);
+      final topLosersData =
+          html.querySelector('#ctl00_ContentPlaceHolder1_LiveLosers')!;
+      final topLosersRow = topLosersData.querySelectorAll('tr');
+      List<Map<String, dynamic>> topLosersMapList = [];
+      for (var row in topLosersRow) {
+        final rowData = row.querySelectorAll('td');
+        Map<String, dynamic> individualMap = {};
+        if (rowData.isNotEmpty) {
+          final title = rowData[0]
+              .querySelector('a')
+              ?.attributes['title']
+              ?.split('(')[1]
+              .replaceAll(")", "");
+          individualMap['companyName'] = title;
+          individualMap['symbol'] = rowData[0].text.toString();
+          individualMap['ltp'] = rowData[1].text.toString();
+          individualMap['change'] = rowData[2].text.toString();
+          individualMap['quantity'] = rowData[6].text.toString();
+        }
+        topLosersMapList.add(individualMap);
       }
-      topLosersMapList.add(individualMap);
+      topLosersMapList.removeAt(0);
+      return {
+        "top_losers": topLosersMapList,
+      };
+    } catch (e) {
+      return {
+        "top_losers": <Map<String, dynamic>>[],
+      };
     }
-    topLosersMapList.removeAt(0);
-    return {"top_losers": topLosersMapList};
   }
 }
