063f0a068f3e4a6e7089a92f0f20e625fa47ec3b
Manil Pun <punmanil3@gmail.com>
2024-03-20 17:31:53 +0545

    replaced streamsubscription with bloclistener on portfolio screen.

--- lib/views/screens/portfolio/portfolio_screen.dart
+++ lib/views/screens/portfolio/portfolio_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,7 +8,6 @@ import 'package:share_portfolio/blocs/portfolio/delete_stock/delete_stock_cubit.
 import 'package:share_portfolio/blocs/portfolio/load_portfolio/load_portfolio_cubit.dart';
 import 'package:share_portfolio/blocs/watchlist/add_to_watchlist/add_to_watchlist_cubit.dart';
 import 'package:share_portfolio/core/constants/string_constants.dart';
-import 'package:share_portfolio/core/di/injection.dart';
 import 'package:share_portfolio/views/screens/portfolio/widgets/current_holdings.dart';
 import 'package:share_portfolio/views/screens/portfolio/widgets/portfolio_item.dart';
 import 'package:share_portfolio/views/screens/portfolio/widgets/portfolio_watchlist_heading.dart';
@@ -26,63 +24,14 @@ class PortfolioScreen extends StatefulWidget {
 }
 
 class _PortfolioScreenState extends State<PortfolioScreen> {
-  StreamSubscription? _deleteStockStreamSubscription;
-  StreamSubscription? _addStockStreamSubscription;
-  StreamSubscription? _addToWatchlistStreamSubscription;
-
   @override
   void initState() {
     super.initState();
     _loadPortfolio();
-    _listenForStockAddDeleteOperation();
-    _listenForWatchlistAddOperation();
-  }
-
-  void _listenForStockAddDeleteOperation() async {
-    _addStockStreamSubscription = getIt<AddStockCubit>().stream.listen((state) {
-      state.whenOrNull(
-        success: () {
-          _loadPortfolio();
-        },
-      );
-    });
-    _deleteStockStreamSubscription =
-        getIt<DeleteStockCubit>().stream.listen((state) {
-      state.whenOrNull(
-        success: () {
-          _loadPortfolio();
-        },
-      );
-    });
-  }
-
-  void _listenForWatchlistAddOperation() async {
-    _addToWatchlistStreamSubscription =
-        getIt<AddToWatchlistCubit>().stream.listen((state) {
-      state.whenOrNull(
-        success: () {
-          _loadPortfolio();
-        },
-      );
-    });
   }
 
   void _loadPortfolio() {
     context.read<LoadPortfolioCubit>().loadPortfolio();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (_deleteStockStreamSubscription != null) {
-      _deleteStockStreamSubscription!.cancel();
-    }
-    if (_addStockStreamSubscription != null) {
-      _addStockStreamSubscription!.cancel();
-    }
-    if (_addToWatchlistStreamSubscription != null) {
-      _addToWatchlistStreamSubscription!.cancel();
-    }
   }
 
   @override
@@ -92,102 +41,133 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
         onRefresh: () async {
           _loadPortfolio();
         },
-        child: BlocBuilder<LoadPortfolioCubit, LoadPortfolioState>(
-          builder: (context, state) {
-            return state.maybeWhen(
-              loading: () => const SpinKitPulsingGrid(
-                color: Colors.white,
-              ),
-              loaded: (
-                totalInvestment,
-                totalShares,
-                totalStock,
-                totalProfiLoss,
-                currentValue,
-                totalPLPercentage,
-                totalDailyPL,
-                localStockDataList,
-                watchlistDataList,
-              ) {
-                return SafeArea(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Welcome(),
-                        CurrentHoldings(
-                          totalProfitLoss: totalProfiLoss,
-                          currentValue: currentValue,
-                          totalSharesCount: totalShares,
-                          totalStockCount: totalStock,
-                        ),
-                        ProfitLoss(
-                          totalInvestment: totalInvestment,
-                          profitLossPercent: totalPLPercentage,
-                          dailyProfitLoss: totalDailyPL,
-                        ),
-                        PortfolioWatchlistHeading(
-                          isListEmpty: localStockDataList.isEmpty,
-                          isPortfolio: true,
-                          title: AppStrings.portfolio,
-                          onViewAll: () {
-                            localStockDataList.isEmpty
-                                ? context.router.push(
-                                    const AddStocksRoute(),
-                                  )
-                                : context.router.push(
-                                    const PortfolioListRouter(),
-                                  );
-                          },
-                        ),
-                        PortfolioItemList(
-                          stockList: localStockDataList,
-                        ),
-                        PortfolioWatchlistHeading(
-                          isListEmpty: watchlistDataList.isEmpty,
-                          title: AppStrings.myWatchlist,
-                          onViewAll: () {
-                            context.router.push(
-                              const WatchlistRoute(),
-                            );
-                          },
-                        ),
-                        WatchlistItemList(
-                          watchlist: watchlistDataList,
-                        ),
-                        const SizedBox(
-                          height: 16.0,
-                        )
-                      ],
-                    ),
-                  ),
+        child: MultiBlocListener(
+          listeners: [
+            BlocListener<AddStockCubit, AddStockState>(
+              listener: (context, state) {
+                state.whenOrNull(
+                  success: () {
+                    _loadPortfolio();
+                  },
                 );
               },
-              failed: () => Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(AppStrings.failedToLoad),
-                    const SizedBox(
-                      height: 12.0,
-                    ),
-                    InkWell(
-                      onTap: () => _loadPortfolio(),
-                      child: const Text(
-                        AppStrings.tapToLoad,
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                        ),
+            ),
+            BlocListener<DeleteStockCubit, DeleteStockState>(
+              listener: (context, state) {
+                state.whenOrNull(
+                  success: () {
+                    _loadPortfolio();
+                  },
+                );
+              },
+            ),
+            BlocListener<AddToWatchlistCubit, AddToWatchlistState>(
+              listener: (context, state) {
+                state.whenOrNull(
+                  success: () {
+                    _loadPortfolio();
+                  },
+                );
+              },
+            ),
+          ],
+          child: BlocBuilder<LoadPortfolioCubit, LoadPortfolioState>(
+            builder: (context, state) {
+              return state.maybeWhen(
+                loading: () => const SpinKitPulsingGrid(
+                  color: Colors.white,
+                ),
+                loaded: (
+                  totalInvestment,
+                  totalShares,
+                  totalStock,
+                  totalProfiLoss,
+                  currentValue,
+                  totalPLPercentage,
+                  totalDailyPL,
+                  localStockDataList,
+                  watchlistDataList,
+                ) {
+                  return SafeArea(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Welcome(),
+                          CurrentHoldings(
+                            totalProfitLoss: totalProfiLoss,
+                            currentValue: currentValue,
+                            totalSharesCount: totalShares,
+                            totalStockCount: totalStock,
+                          ),
+                          ProfitLoss(
+                            totalInvestment: totalInvestment,
+                            profitLossPercent: totalPLPercentage,
+                            dailyProfitLoss: totalDailyPL,
+                          ),
+                          PortfolioWatchlistHeading(
+                            isListEmpty: localStockDataList.isEmpty,
+                            isPortfolio: true,
+                            title: AppStrings.portfolio,
+                            onViewAll: () {
+                              localStockDataList.isEmpty
+                                  ? context.router.push(
+                                      const AddStocksRoute(),
+                                    )
+                                  : context.router.push(
+                                      const PortfolioListRouter(),
+                                    );
+                            },
+                          ),
+                          PortfolioItemList(
+                            stockList: localStockDataList,
+                          ),
+                          PortfolioWatchlistHeading(
+                            isListEmpty: watchlistDataList.isEmpty,
+                            title: AppStrings.myWatchlist,
+                            onViewAll: () {
+                              context.router.push(
+                                const WatchlistRoute(),
+                              );
+                            },
+                          ),
+                          WatchlistItemList(
+                            watchlist: watchlistDataList,
+                          ),
+                          const SizedBox(
+                            height: 16.0,
+                          )
+                        ],
                       ),
                     ),
-                  ],
+                  );
+                },
+                failed: () => Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(AppStrings.failedToLoad),
+                      const SizedBox(
+                        height: 12.0,
+                      ),
+                      InkWell(
+                        onTap: () => _loadPortfolio(),
+                        child: const Text(
+                          AppStrings.tapToLoad,
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              orElse: () {
-                return Container();
-              },
-            );
-          },
+                orElse: () {
+                  return Container();
+                },
+              );
+            },
+          ),
         ),
       ),
     );
